0fc17d27a96f48650145b3956a5af976538588cf
TKM <mosestkamau@gmail.com>
2022-04-21 08:15:13 -0700

    Arguments and Parameters [passing arguments]

---
+++ args-params_functions.py
@@ -0,0 +1,8 @@
+"""Arguments and Parameters"""
+"""Positional Arguments"""
+def describe_animal(animal_type, horse_name):
+    """Display information about a pet"""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {horse_name.title()}.")
+
+describe_animal('horse', 'senior mustang')
