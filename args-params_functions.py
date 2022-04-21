fcf24bb031b7d9fd880deb63b940db295dd04f4c
TKM <mosestkamau@gmail.com>
2022-04-21 08:17:07 -0700

    Changed a comment

--- args-params_functions.py
+++ args-params_functions.py
@@ -1,7 +1,7 @@
 """Arguments and Parameters"""
 """Positional Arguments"""
 def describe_animal(animal_type, horse_name):
-    """Display information about a pet"""
+    """Display information about a horse"""
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {horse_name.title()}.")
 
