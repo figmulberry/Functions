45a7c6b288262219ffee021682ccde6329a70079
TKM <mosestkamau@gmail.com>
2022-04-22 15:40:23 -0700

    Making an Argument Optional [middle-name] in a function

--- make-args-optional_functions.py
+++ make-args-optional_functions.py
@@ -1,7 +1,28 @@
-def get_formatted_name(first_name, middle_name, last_name):
+# def get_formatted_name(first_name, middle_name, last_name):
+#     """Return a full name, neatly formatted."""
+#     full_name = f"{first_name} {middle_name} {last_name}"
+#     return full_name.title()
+#
+# developer = get_formatted_name('fig', '-', 'mulberry')
+# print(developer)
+
+"""**************************************************************"""
+# Middle names aren't always needed
+# Make a middle name optional
+"""
+The name is build in three possible parts, (first, middle, last) names.
+Python matches up the positional arguments correctly.
+"""
+def get_formatted_name(first_name, last_name, middle_name=''):
     """Return a full name, neatly formatted."""
-    full_name = f"{first_name} {middle_name} {last_name}"
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
     return full_name.title()
+
+developer = get_formatted_name('fig', 'mulberry')
+print(developer)
 
 developer = get_formatted_name('fig', '-', 'mulberry')
 print(developer)
